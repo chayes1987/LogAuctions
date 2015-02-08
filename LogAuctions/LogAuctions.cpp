7e78280649927f571eee9a41f05e2a8f32aa7f70
Conor Hayes <ch1987@live.ie>
2015-02-08 13:38:45 +0000

    Added library for 0mq integration. Initialized context and subscriber

--- LogAuctions/LogAuctions.cpp
+++ LogAuctions/LogAuctions.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
+#include <zmq.hpp>
 
-
-int _tmain(int argc, _TCHAR* argv[])
-{
+int _tmain(int argc, _TCHAR* argv[]){
+	zmq::context_t context(1);
+	zmq::socket_t subscriber(context, ZMQ_SUB);
 	return 0;
 }
 
